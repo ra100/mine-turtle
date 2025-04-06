a369f49f99b38172b1a14e5a26a12833e418dce3
ra100 <git@ra100.net>
2025-04-06 14:25:00 +0200

    Rename press button to explode button and update related functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const pressButton = document.getElementById('press-button')
+const explodeButton = document.getElementById('explode-button')
 const statusDisplay = document.getElementById('status')
 const turtleImage = document.getElementById('turtle-image')
 
@@ -14,7 +14,7 @@ function checkExplosion() {
     exploded = true
     turtleImage.src = 'explosion.svg'
     statusDisplay.textContent = 'BOOM! The Mine Turtle exploded!'
-    pressButton.disabled = true
+    explodeButton.disabled = true
     setTimeout(resetGame, 2000) // Reset the game after 2 seconds
   } else {
     statusDisplay.textContent = 'Safe... for now.'
@@ -24,21 +24,24 @@ function checkExplosion() {
 function resetGame() {
   pressCount = 0
   exploded = false
-  maxPresses = Math.floor(Math.random() * (200 - 50 + 1)) + 50 // Random value between 50 and 200
+  maxPresses = Math.floor(Math.random() * 200) + 100 // Random value between 50 and 200
   turtleImage.src = 'turtle.png'
   statusDisplay.textContent = "Don't let it explode!"
-  pressButton.disabled = false
+  explodeButton.disabled = false
 }
 
-pressButton.addEventListener('click', () => {
+function handleTouchStart() {
+  statusDisplay.textContent = "Don't let it explode!"
+}
+
+explodeButton.addEventListener('click', () => {
   if (!exploded) {
     pressCount++
-    pressButton.classList.add('pressed')
-    setTimeout(() => {
-      checkExplosion()
-      pressButton.classList.remove('pressed')
-    }, 500)
+    checkExplosion()
   }
 })
+
+explodeButton.addEventListener('mousedown', handleTouchStart)
+explodeButton.addEventListener('touchstart', handleTouchStart)
 
 resetGame()
